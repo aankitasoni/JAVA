5ca0772a500cbea61452a2308f8fc3fe3ed8db79
Ankita Soni <ankitasoni018@gmail.com>
2023-04-14 22:08:17 +0530

    Decoder

---
+++ src/Dcoder/Dcoder.java
@@ -0,0 +1,18 @@
+package Dcoder;
+
+import java.util.Scanner;
+
+public class Dcoder {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print("Dc");
+        for (int i = 1; i <= n; i++) {
+            System.out.print("o");
+        }
+        System.out.print("der");
+    }
+
+
+}
