6ff978c2e729aa93ac40be301a3c6c3d42ee1a4d
Ankita Soni <ankitasoni018@gmail.com>
2023-04-14 23:32:27 +0530

    Printing Number Using Loop

---
+++ src/Dcoder/Printing_Number_Using_Loop.java
@@ -0,0 +1,11 @@
+package Dcoder;
+
+public class Printing_Number_Using_Loop {
+    public static void main(String args[])
+    {
+        int i = 10;
+        for (int a = 1; a <= i; a++){
+            System.out.println(a);
+        }
+    }
+}
