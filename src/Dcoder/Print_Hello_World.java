d4e3d8b9361efbad8eb31480c5048eb3767eec5b
Ankita Soni <ankitasoni018@gmail.com>
2023-04-14 23:23:40 +0530

    Print Hello World

---
+++ src/Dcoder/Print_Hello_World.java
@@ -0,0 +1,12 @@
+package Dcoder;
+
+import java.util.Scanner;
+
+public class Print_Hello_World {
+
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+    }
+
+
+}
