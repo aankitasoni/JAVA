43e61a44042611ff75dfe9553cd8968537b5d215
Ankita Soni <ankitasoni018@gmail.com>
2023-04-16 00:46:25 +0530

    Dcooooooooooder Fan

---
+++ src/Dcoder/Dcooooooooooder_Fan.java
@@ -0,0 +1,13 @@
+package Dcoder;
+import java.util.*;
+public class Dcooooooooooder_Fan {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print("Dc");
+        for (int i =1; i <=n ; i++){
+            System.out.print("o");
+        }
+        System.out.print("der");
+    }
+}
