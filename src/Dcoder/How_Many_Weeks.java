9c9b1f5bcaac1dcbab5fd6358111ace965045dce
Ankita Soni <ankitasoni018@gmail.com>
2023-04-16 00:04:57 +0530

    How Many Weeks

---
+++ src/Dcoder/How_Many_Weeks.java
@@ -0,0 +1,10 @@
+package Dcoder;
+import java.util.*;
+public class How_Many_Weeks {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println((int)10000/n);
+    }
+}
