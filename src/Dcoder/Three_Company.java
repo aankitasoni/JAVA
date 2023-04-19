1d398d1fbfd7cf2a1d80555fd012d3be9ad35369
Ankita Soni <ankitasoni018@gmail.com>
2023-04-19 11:21:05 +0530

    Three's Company

---
+++ src/Dcoder/Three_Company.java
@@ -0,0 +1,17 @@
+package Dcoder;
+import java.util.*;
+public class Three_Company {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String s = sc.next();
+        int p = sc.nextInt();
+        for (int i =0; i < s.length(); i++){
+            for (int a = 0; a < p ; a++){
+                System.out.print(s.charAt(i));
+            }
+        }
+    }
+
+
+}
