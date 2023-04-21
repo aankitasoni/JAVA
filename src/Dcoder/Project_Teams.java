aa695c7570242cd114838933c05f6d356eedbb9a
Ankita Soni <ankitasoni018@gmail.com>
2023-04-21 23:26:17 +0530

    Project Teams

---
+++ src/Dcoder/Project_Teams.java
@@ -0,0 +1,11 @@
+package Dcoder;
+import java.util.*;
+public class Project_Teams {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int g = 3;
+        System.out.println((int) n / g);
+    }
+}
