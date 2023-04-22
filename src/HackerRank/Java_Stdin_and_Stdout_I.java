97832ce67c743f08c48330aac9f15ae446b6bb82
Ankita Soni <ankitasoni018@gmail.com>
2023-04-22 22:35:55 +0530

    Java Stdin and Stdout I

---
+++ src/HackerRank/Java_Stdin_and_Stdout_I.java
@@ -0,0 +1,14 @@
+package HackerRank;
+import java.io.*;
+import java.util.*;
+
+public class Java_Stdin_and_Stdout_I {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 3;
+        for (int i = 1; i<= n; i++){
+            int p = sc.nextInt();
+            System.out.println(p);
+        }
+    }
+}
