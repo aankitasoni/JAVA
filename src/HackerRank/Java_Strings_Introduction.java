2c0b57540ff9353cea9d3e4337edd00e7bc3cd2a
Ankita Soni <ankitasoni018@gmail.com>
2023-04-24 00:42:45 +0530

    Java Strings Introduction

---
+++ src/HackerRank/Java_Strings_Introduction.java
@@ -0,0 +1,22 @@
+package HackerRank;
+import java.io.*;
+import java.util.*;
+
+public class Java_Strings_Introduction {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        System.out.println(a.length() + b.length());
+        if (a.compareTo(b) > 0){
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+        String s1 = String.valueOf(a.charAt(0));
+        String s2 = String.valueOf(b.charAt(0));
+        s1 = s1.toUpperCase();
+        s2 = s2.toUpperCase();
+        System.out.println(s1 +  a.substring(1) + " "+ s2 + b.substring(1));
+    }
+}
