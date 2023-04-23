6c29b6099bfe86ea2aa9c50608e401296b244752
Ankita Soni <ankitasoni018@gmail.com>
2023-04-23 23:03:59 +0530

    Java Stdin and Stdout II

---
+++ src/HackerRank/Java_Stdin_and_Stdout_II.java
@@ -0,0 +1,15 @@
+package HackerRank;
+import java.util.Scanner;
+
+public class Java_Stdin_and_Stdout_II {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        double d = sc.nextDouble();
+        sc.nextLine();
+        String s  = sc.nextLine();
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
