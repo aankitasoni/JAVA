1d88e8ebbdb28591ce8949c6dc21cad6078429b2
Ankita Soni <ankitasoni018@gmail.com>
2023-04-24 01:31:01 +0530

    Java Strings Introduction

--- src/HackerRank/Java_Strings_Introduction.java
+++ src/HackerRank/Java_Strings_Introduction.java
@@ -1,7 +1,5 @@
 package HackerRank;
-import java.io.*;
 import java.util.*;
-
 public class Java_Strings_Introduction {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
