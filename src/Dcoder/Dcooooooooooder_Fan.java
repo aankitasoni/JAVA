2a301d7fedd014ac6d11016d0c90ca5ea3b84a5e
Ankita Soni <92040840+aankitasoni@users.noreply.github.com>
2023-05-22 21:40:20 +0530

    Dcooooooooooder_Fan

--- src/Dcoder/Dcooooooooooder_Fan.java
+++ src/Dcoder/Dcooooooooooder_Fan.java
@@ -1,6 +1,7 @@
 package Dcoder;
 import java.util.*;
 public class Dcooooooooooder_Fan {
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
