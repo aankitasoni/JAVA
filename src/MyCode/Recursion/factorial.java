4afb4411afdb193c83c65effb787f9369a568648
Ankita Soni <ankitasoni018@gmail.com>
2023-09-30 23:04:22 +0530

    factorial

---
+++ src/MyCode/Recursion/factorial.java
@@ -0,0 +1,12 @@
+package MyCode.Recursion;
+
+public class factorial {
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(fac(n));
+    }
+    public static int fac(int n){
+        if(n == 0) return 1;
+        return (n * fac(n-1));
+    }
+}
